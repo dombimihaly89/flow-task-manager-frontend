551bdfed867d6fc53e69295305110ffc39aaaa6b
Mihaly Dombi <dombimihaly89@gmail.com>
2020-01-16 18:50:41 +0100

    User Class skeleton is done

--- src/main/java/hu/flowacademy/flowtaskmanager/models/User.java
+++ src/main/java/hu/flowacademy/flowtaskmanager/models/User.java
@@ -1,4 +1,32 @@
 package hu.flowacademy.flowtaskmanager.models;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class User {
+
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dateOfBirth;
+
+    private List<Task> tasks;
+
+    private List<Comment> lists;
+
+    private List<Solution> solutions;
+
+    public enum Role {
+        MENTOR,
+        STUDENT
+    }
+
+
 }
