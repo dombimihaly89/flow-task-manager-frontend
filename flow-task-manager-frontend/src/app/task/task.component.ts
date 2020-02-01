9445b254b77aa59abae3b58dbdf2e71aebbac261
Mihaly Dombi <dombimihaly89@gmail.com>
2020-02-01 14:41:51 +0100

    inserted some style in task component

--- flow-task-manager-frontend/src/app/task/task.component.ts
+++ flow-task-manager-frontend/src/app/task/task.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TaskService } from 'src/app/services/taskservice/task.service';
+import { Task } from 'src/app/models/task';
 
 @Component({
   selector: 'app-task',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  tasks : any;
+
+  ratingAverage: number;
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.taskService.getTasks().subscribe(
+      (data) => {
+        this.tasks = data;
+      }
+    )
+    console.log(this.tasks);
+  }
+
+  getRating(task) {
+    let list = task.ratings;
+    let sum : number = 0;
+    for (let i = 0; i < list.length; i++) {
+      sum += list[i];
+    }
+    this.ratingAverage = sum / list.length;
+    return this.ratingAverage;
   }
 
 }
--- flow-task-manager-frontend/src/app/task/tasklist/tasklist.component.ts
+++ flow-task-manager-frontend/src/app/task/tasklist/tasklist.component.ts
@@ -9,28 +9,9 @@ import { Task } from 'src/app/models/task';
 })
 export class TasklistComponent implements OnInit {
 
-  tasks : any;
-  ratingAverage: number;
-
   constructor(private taskService: TaskService) { }
 
   ngOnInit() {
-    this.taskService.getTasks().subscribe(
-      (data) => {
-        this.tasks = data;
-      }
-    )
-    console.log(this.tasks);
-  }
-
-  getRating(task) {
-    let list = task.ratings;
-    let sum : number = 0;
-    for (let i = 0; i < list.length; i++) {
-      sum += list[i];
-    }
-    this.ratingAverage = sum / list.length;
-    return this.ratingAverage;
   }
 
 }
